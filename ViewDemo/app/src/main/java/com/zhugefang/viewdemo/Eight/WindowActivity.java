4ae6349f108994308ca57c2abd927edff8891044
wangchunlin <wangchunlin@zhugefang.com>
2018-01-15 02:50:52 +0800

    Window内部机制

--- ViewDemo/app/src/main/java/com/zhugefang/viewdemo/Eight/WindowActivity.java
+++ ViewDemo/app/src/main/java/com/zhugefang/viewdemo/Eight/WindowActivity.java
@@ -6,12 +6,19 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import com.zhugefang.viewdemo.R;
 
 public class WindowActivity extends AppCompatActivity {
+    String TAG = "WindowActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +33,40 @@ public class WindowActivity extends AppCompatActivity {
         //setContentView(R.layout.activity_window);
         //WindowManager.LayoutParams是继承自ViewGroup.LayoutParams,Window是一个抽象的类，不能new,
         //所以添加view的时候，都是2个参数，view和params的形式
-        TextView button = new TextView(this);
+        final TextView button = new TextView(this);
         button.setText("button");
         button.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-        WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(500,500, 0, 0, PixelFormat.TRANSPARENT);
+        final WindowManager.LayoutParams layoutParams = new WindowManager.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT, 0, 0, PixelFormat.TRANSPARENT);
         //flag设置window属性
-        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;
+        layoutParams.flags = WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL;//onTouch事件可以，onClick事件不可以
         // type 设置 Window 类别（层级），这个决定是window中的哪三样：应用window\子window\系统window
-        layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_OVERLAY;
-        layoutParams.gravity = Gravity.CENTER;
-        WindowManager windowManager = getWindowManager();
+        layoutParams.type = WindowManager.LayoutParams.TYPE_SYSTEM_ERROR;
+//        layoutParams.gravity = Gravity.TOP;
+        final WindowManager windowManager = getWindowManager();
         windowManager.addView(button, layoutParams);
+        button.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+//                Toast.makeText(WindowActivity.this, "button", Toast.LENGTH_LONG).show();
+//                layoutParams.y=100;
+//                layoutParams.x=800;
+//                windowManager.updateViewLayout(button,layoutParams);
+                windowManager.removeView(button);
+                float rawX = event.getRawX();
+                float rawY = event.getRawY();
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_MOVE:
+                        layoutParams.x = ((int) rawX);
+                        layoutParams.y = ((int) rawY);
+                        Log.d(TAG, "onTouch: layoutParams.x--"+layoutParams.x+"--rawX--"+rawX);
+                        Log.d(TAG, "onTouch: layoutParams.y--"+layoutParams.y+"--rawY--"+rawY);
+                        windowManager.updateViewLayout(button, layoutParams);
+                        break;
+                }
+//                Toast.makeText(WindowActivity.this, "button", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
 }

8841eb0acb409beae8af4184d54800c7d054a4bc
wangchunlin <wangchunlin@zhugefang.com>
2018-01-24 00:35:53 +0800

    窗口机制

--- ViewDemo/app/src/main/java/com/zhugefang/viewdemo/Eight/Main4Activity.java
+++ ViewDemo/app/src/main/java/com/zhugefang/viewdemo/Eight/Main4Activity.java
@@ -1,8 +1,13 @@
 package com.zhugefang.viewdemo.Eight;
 
 import android.app.Dialog;
+import android.graphics.PixelFormat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.zhugefang.viewdemo.R;
 
@@ -10,9 +15,13 @@ public class Main4Activity extends WindowActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
-        Dialog dialog = new Dialog(this);
-        dialog.cancel();
+        getWindow().getDecorView();
+//        Button button = new Button(this);
+//        button.setText("button");
+//        new WindowManager.LayoutParams(WindowManager.LayoutParams.WRAP_CONTENT, WindowManager.LayoutParams.WRAP_CONTENT,
+//                0,0, PixelFormat.TRANSPARENT);
     }
 }
